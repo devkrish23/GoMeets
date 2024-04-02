9361b18c60470a8ef68c15c9caad0232d5d91451
KrishnaKumar23 <krishna158@live.com>
2024-04-01 20:45:51 -0400

    Updated chat room code

--- internal/handlers/chat.go
+++ internal/handlers/chat.go
@@ -12,6 +12,23 @@ func RoomChat(c *fiber.Ctx) error {
 	return c.Render("chat", fiber.Map{}, "layouts/main")
 }
 
+func RoomChatWebsocket(c *websocket.Conn) {
+	uuid := c.Params("uuid")
+	if uuid == "" {
+		return
+	}
+
+	w.RoomsLock.Lock()
+	room := w.Rooms[uuid]
+	w.RoomsLock.Unlock()
+	if room == nil {
+		return
+	}
+	if room.Hub == nil {
+		return
+	}
+	chat.PeerChatConn(c.Conn, room.Hub)
+}
 
 func StreamChatWebsocket(c *websocket.Conn) {
 	suuid := c.Params("suuid")
