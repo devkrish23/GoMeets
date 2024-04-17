8163a15cadbd17b9a6063e7037dacd4245807b82
Krishna Kumar Yadav <krishna158@live.com>
2024-04-16 22:11:55 -0400

    Update main.go

--- main.go
+++ main.go
@@ -1,40 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"os"
-	"os/exec"
+
 	"v/internal/server"
 )
 
 func main() {
-
-	packagePath := "github.com/pion/webrtc/v3"
-
-	// Execute the `go get` command
-	cmd := exec.Command("go", "get", packagePath)
-
-	// Set output to os.Stdout to see output in the terminal
-	cmd.Stdout = os.Stdout
-
-	// Set error output to os.Stderr to see errors in the terminal
-	cmd.Stderr = os.Stderr
-
-	// Run the command
-	err := cmd.Run()
-	if err != nil {
-		fmt.Printf("Error running go get: %v\n", err)
-		return
-	}
-
-	fmt.Println("Package successfully downloaded:", packagePath)
-
-
-
-
-
-	
 	if err := server.Run(); err != nil {
 		log.Fatalln(err.Error())
 	}
